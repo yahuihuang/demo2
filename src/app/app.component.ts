e95e7af04a176e0ba77563b065fbc9a18e3d40f6
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-07-05 14:55:20 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo2';
+  keyword = '請輸入';
+  doSearch(str: string): void {
+    this.keyword = str;
+  }
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  sitename = '換網站名稱';
+  subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
+  fontsize = 12;
+  constructor() { }
+
+  ngOnInit(): void {
+    /*setTimeout(() => {
+      this.sitename = "這是我的測試網站";
+    }, 2000);*/
+  }
+
+  changeSitename(eventData: MouseEvent): void {
+    console.log(eventData);
+    this.fontsize += 1;
+    if (eventData.ctrlKey === true) {
+      this.sitename = '這是我的測試網站';
+    }
+  }
+}
